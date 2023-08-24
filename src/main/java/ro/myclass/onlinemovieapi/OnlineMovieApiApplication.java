dbcfa0231a04da5f4c7657178773ed23e8f174c0
7raul7k <balanescuraul@gmail.com>
2023-08-24 11:46:37 +0300

    add main

---
+++ src/main/java/ro/myclass/onlinemovieapi/OnlineMovieApiApplication.java
@@ -0,0 +1,13 @@
+package ro.myclass.onlinemovieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/myclass/onlinemovieapi/OnlineMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.myclass.onlinemovieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
